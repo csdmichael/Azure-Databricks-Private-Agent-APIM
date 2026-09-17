bce24abf3648e4b502d7f3dce9633d05d7d95d5f
Michael Yaacoub <csdmichael@gmail.com>
2026-09-17 09:45:11 -0700

    Move Fabric OBO solution to dedicated repository

--- fabric/functions/obo-broker/src/errors.ts
+++
@@ -1,9 +0,0 @@
-export class BrokerError extends Error {
-  constructor(public readonly status: number, public readonly code: string) {
-    super(code);
-  }
-}
-
-export function safeError(error: unknown): BrokerError {
-  return error instanceof BrokerError ? error : new BrokerError(503, 'broker_unavailable');
-}
